76f75b77cdc7c696818cbe680fb9806250ca5d16
MUHAMMMMED <amer66965@gmail.com>
2025-11-14 00:13:48 +0200

    Add PaymentHeader to Checkout, update Pixel tracking and Contact Form

--- frontend/src/pages/Cart/components/Checkout/Checkout.jsx
+++ frontend/src/pages/Cart/components/Checkout/Checkout.jsx
@@ -72,7 +72,7 @@ export default function Checkout() {
                         stripe={stripePromise}
                         options={{
                             fetchClientSecret,
-                            locale: 'ar',
+                            // locale: 'ar',
                         }}>
                         <EmbeddedCheckout />
                     </EmbeddedCheckoutProvider>
